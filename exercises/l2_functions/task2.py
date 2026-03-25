44a4d2034a327c9d4555c44bbcf8656243764f88
kukamemas <kukamemas@gmail.com>
2026-03-26 00:06:20 +0300

    l2: completed task 2

--- exercises/l2_functions/task2.py
+++ exercises/l2_functions/task2.py
@@ -5,7 +5,8 @@
 # by whitespace ' '.
 #
 # For example, call of `join("a", "b")` should return "a b"
-
+def join(a: str, b: str) -> str:
+    return a + " " + b
 
 # Do not change the below's code
 if __name__ == "__main__":
