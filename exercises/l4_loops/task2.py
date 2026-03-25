5aff30edd2ba977b2bc21c2fe493cb9378fab82f
kukamemas <kukamemas@gmail.com>
2026-03-26 01:57:33 +0300

    l4: completed task 2

--- exercises/l4_loops/task2.py
+++ exercises/l4_loops/task2.py
@@ -1,5 +1,9 @@
 def odd_str(n: int) -> str:
     s = ""
+    for i in range(1, n+1, 2):
+        s += str(i)
+    return s
+
 
     # Use for loop to run from 0 to n (included) 
     # and form a string `s`.
@@ -9,7 +13,6 @@ def odd_str(n: int) -> str:
     # (0, 1, 2, 3, 4) -> "13"
     # (0, 1, 2, 3, 4, 5, 6) -> "135" 
 
-    return s
 
 
 # Do not change the below's code
